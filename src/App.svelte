57a80046c0a68c6ee194c3b697b4110c29b8c696
truizdiaz <tomiruizdiaz@gmail.com>
2020-11-16 15:36:50 -0300

    Parte 1

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Navbar from './components/Navbar.svelte';
+	import Convertidor from './components/Convertidor.svelte';
+</script>
+
+<main>
+	<Navbar />
+	<Convertidor />
+</main>
+
+<style>
+	
+</style>
---
+++ src/components/Convertidor.svelte
@@ -0,0 +1,39 @@
+<script>
+
+</script>
+
+<div>
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="form-group">
+                    <label for="cantidad">Ingrese cantidad</label>
+                    <input type="numer" class="form-control" id="cantidad" placeholder="1000">
+                  </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="form-group">
+                    <label for="tengo">Tengo</label>
+                    <select class="form-control" id="tengo">
+                      <option>USD</option>
+                      <option>EURO</option>
+                    </select>
+                  </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="form-group">
+                    <label for="quiero">Quiero</label>
+                    <select class="form-control" id="quiero">
+                      <option>USD</option>
+                      <option>EURO</option>
+                    </select>
+                  </div>
+            </div>
+        </div>
+    </div>
+</div>
+<style>
+
+</style>
