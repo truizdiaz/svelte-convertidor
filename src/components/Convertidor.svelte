517ffb345bdbc4b5d39f372eaa8c81ece9f911f4
truizdiaz <tomiruizdiaz@gmail.com>
2020-11-16 15:49:45 -0300

    Parte 2

--- src/components/Convertidor.svelte
+++ src/components/Convertidor.svelte
@@ -1,6 +1,13 @@
 <script>
-
+    let tengo = "USD";
+    let quiero = "EUR";
+    let cantidad = 0;
+    let total = 0;
+    let monedas = ["USD", "EUR", "LIBRA"];
 </script>
+
+<style>
+</style>
 
 <div>
     <div class="container mt-2">
@@ -8,32 +15,38 @@
             <div class="col-lg-12">
                 <div class="form-group">
                     <label for="cantidad">Ingrese cantidad</label>
-                    <input type="numer" class="form-control" id="cantidad" placeholder="1000">
-                  </div>
+                    <input
+                        type="numer"
+                        class="form-control form-control-lg"
+                        id="cantidad"
+                        placeholder="1000" />
+                </div>
             </div>
         </div>
         <div class="row">
             <div class="col-lg-6">
                 <div class="form-group">
                     <label for="tengo">Tengo</label>
-                    <select class="form-control" id="tengo">
-                      <option>USD</option>
-                      <option>EURO</option>
+                    <select class="form-control form-control-lg" id="tengo" bind:value={tengo}>
+                        {#each monedas as moneda}
+                            <option>{ moneda }</option>
+                        {/each}
                     </select>
-                  </div>
+                </div>
             </div>
             <div class="col-lg-6">
                 <div class="form-group">
                     <label for="quiero">Quiero</label>
-                    <select class="form-control" id="quiero">
-                      <option>USD</option>
-                      <option>EURO</option>
+                    <select class="form-control form-control-lg" id="quiero" bind:value={quiero}>
+                        {#each monedas as moneda}
+                            <option>{ moneda }</option>
+                        {/each}
                     </select>
-                  </div>
+                </div>
             </div>
         </div>
+        <h4 class="text-center">
+            {cantidad} {tengo} SON {total} {quiero}
+        </h4>
     </div>
 </div>
-<style>
-
-</style>
