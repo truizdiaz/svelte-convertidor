1e4cd79a8bfbaeb7471fcbfc7fd840ccc1852a22
truizdiaz <tomiruizdiaz@gmail.com>
2020-11-16 16:05:29 -0300

    App Finalizada

--- src/components/Convertidor.svelte
+++ src/components/Convertidor.svelte
@@ -4,6 +4,44 @@
     let cantidad = 0;
     let total = 0;
     let monedas = ["USD", "EUR", "LIBRA"];
+
+    function convertir() {
+        switch (tengo) {
+            case "USD":
+                if (quiero == "USD") {
+                    total = cantidad;
+                }
+                if (quiero == "EUR") {
+                    total = cantidad * 0.84;
+                }
+                if (quiero == "LIBRA") {
+                    total = cantidad * 0.76;
+                }
+                break;
+            case "EUR":
+                if (quiero == "USD") {
+                    total = cantidad * 1.18;
+                }
+                if (quiero == "EUR") {
+                    total = cantidad;
+                }
+                if (quiero == "LIBRA") {
+                    total = cantidad * 0.9;
+                }
+                break;
+            case "LIBRA":
+                if (quiero == "USD") {
+                    total = cantidad * 1.32;
+                }
+                if (quiero == "EUR") {
+                    total = cantidad * 1.11;
+                }
+                if (quiero == "LIBRA") {
+                    total = cantidad;
+                }
+                break;
+        }
+    }
 </script>
 
 <style>
@@ -19,7 +57,8 @@
                         type="numer"
                         class="form-control form-control-lg"
                         id="cantidad"
-                        placeholder="1000" />
+                        bind:value={cantidad}
+                        on:input={() => convertir()} />
                 </div>
             </div>
         </div>
@@ -27,9 +66,14 @@
             <div class="col-lg-6">
                 <div class="form-group">
                     <label for="tengo">Tengo</label>
-                    <select class="form-control form-control-lg" id="tengo" bind:value={tengo}>
+                    <!-- svelte-ignore a11y-no-onchange -->
+                    <select
+                        class="form-control form-control-lg"
+                        id="tengo"
+                        bind:value={tengo}
+                        on:change={() => convertir()}>
                         {#each monedas as moneda}
-                            <option>{ moneda }</option>
+                            <option>{moneda}</option>
                         {/each}
                     </select>
                 </div>
@@ -37,16 +81,21 @@
             <div class="col-lg-6">
                 <div class="form-group">
                     <label for="quiero">Quiero</label>
-                    <select class="form-control form-control-lg" id="quiero" bind:value={quiero}>
+                    <!-- svelte-ignore a11y-no-onchange -->
+                    <select
+                        class="form-control form-control-lg"
+                        id="quiero"
+                        bind:value={quiero}
+                        on:change={() => convertir()}>
                         {#each monedas as moneda}
-                            <option>{ moneda }</option>
+                            <option>{moneda}</option>
                         {/each}
                     </select>
                 </div>
             </div>
         </div>
-        <h4 class="text-center">
-            {cantidad} {tengo} SON {total} {quiero}
-        </h4>
+        {#if cantidad > 0}
+            <h4 class="text-center">{cantidad} {tengo} SON {total.toFixed(2)} {quiero}</h4>
+        {/if}
     </div>
 </div>
